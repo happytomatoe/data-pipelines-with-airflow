78f740473a279c7bd9cd14dc7e77507b571a1a04
Roman Lukash <v-roman.lukash@choreograph.com>
2021-10-19 01:18:57 +0300

    WIP

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -1,11 +1,15 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from textwrap import dedent
 
 from airflow import DAG
+from airflow.operators import (StageToRedshiftOperator)
 from airflow.operators.dummy_operator import DummyOperator
-# from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-#                                LoadDimensionOperator, DataQualityOperator,
-#                                PostgresOperator)
 from airflow.operators.postgres_operator import PostgresOperator
+
+S3_BUCKET = "udacity-dend"
+
+LOG_DATA_S3_KEY = "log_data"
+SONG_DATA_S3_KEY = "song_data/A/A/C"
 
 default_args = {
     'owner': 'udacity',
@@ -14,6 +18,11 @@ default_args = {
 
 # Copied from https://stackoverflow.com/questions/42982986/external-files-in-airflow-dag/46091929#46091929
 # tmpl_search_path = Variable.get("sql_path")
+# The DAG does not have dependencies on past runs
+# On failure, the task are retried 3 times
+# Retries happen every 5 minutes
+# Catchup is turned off
+# Do not email on retry
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
@@ -31,15 +40,40 @@ create_tables = PostgresOperator(
     sql="sql/create_tables.sql"
 )
 
-# stage_events_to_redshift = StageToRedshiftOperator(
-#     task_id='Stage_events',
-#     dag=dag
-# )
-#
-# stage_songs_to_redshift = StageToRedshiftOperator(
-#     task_id='Stage_songs',
-#     dag=dag
-# )
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    s3_bucket=S3_BUCKET,
+    s3_key=LOG_DATA_S3_KEY,
+    schema="PUBLIC",
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_conn_id="aws_credentials",
+    copy_options=dedent("""
+    COMPUPDATE OFF STATUPDATE OFF
+    FORMAT AS JSON 'auto ignorecase'
+    TIMEFORMAT AS 'epochmillisecs'
+    TRUNCATECOLUMNS
+    BLANKSASNULL;
+    """),
+    dag=dag
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    s3_bucket=S3_BUCKET,
+    s3_key=SONG_DATA_S3_KEY,
+    schema="PUBLIC",
+    table="staging_songs",
+    redshift_conn_id="redshift",
+    aws_conn_id="aws_credentials",
+    copy_options=dedent("""
+    COMPUPDATE OFF STATUPDATE OFF
+    FORMAT AS JSON 'auto'
+    TRUNCATECOLUMNS
+    BLANKSASNULL;
+    """),
+    dag=dag
+)
 #
 # load_songplays_table = LoadFactOperator(
 #     task_id='Load_songplays_fact_table',
@@ -73,7 +107,7 @@ create_tables = PostgresOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
 
-start_operator >> create_tables >> end_operator
+start_operator >> stage_events_to_redshift >> end_operator
 #     [stage_songs_to_redshift]
 # ,stage_events_to_redshift
 # >> load_songplays_table \
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,51 @@
+import logging
+from textwrap import dedent
+
+from airflow.hooks.base_hook import BaseHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+
+COPY_QUERY_TEMPLATE = dedent("""
+            COPY {schema}.{table}
+            FROM 's3://{s3_bucket}/{s3_key}'
+            with credentials
+            'aws_access_key_id={access_key};aws_secret_access_key={secret_key}'
+            {copy_options}
+        """)
+
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
                  *args, **kwargs):
-
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = kwargs['redshift_conn_id']
+        self.aws_conn_id = kwargs['aws_conn_id']
+        self.s3_bucket = kwargs['s3_bucket']
+        self.s3_key = kwargs['s3_key']
+        self.schema = kwargs['schema']
+        self.table = kwargs['table']
+        self.copy_options = kwargs['copy_options']
+
+        self.hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_connection = BaseHook.get_connection(self.aws_conn_id)
+        logging.info(f"Aws aws_connection login {aws_connection.login}")
+        copy_query = COPY_QUERY_TEMPLATE.format(schema=self.schema,
+                                                table=self.table,
+                                                s3_bucket=self.s3_bucket,
+                                                s3_key=self.s3_key,
+                                                access_key=aws_connection.login,
+                                                secret_key=aws_connection.password,
+                                                copy_options=self.copy_options)
+        self.log.info(f"Executing {copy_query}")
 
+        self.hook.run(copy_query, True)
+        for output in self.hook.conn.notices:
+            self.log.info(output)
 
-
-
-
+        self.log.info('Finished')
