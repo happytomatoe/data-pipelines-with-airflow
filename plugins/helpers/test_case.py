3489cc4313fac33725efddf035e93a04a8938799
Roman Lukash <v-roman.lukash@choreograph.com>
2021-10-20 17:29:48 +0300

    WIP

--- plugins/helpers/test_case.py
+++ plugins/helpers/test_case.py
@@ -1,17 +1,14 @@
-# import typing
-# from dataclasses import dataclass
-from collections import namedtuple
+import typing
+from dataclasses import dataclass
 
-TestCase = namedtuple("TestCase", "sql expected_value")
-
-# @dataclass
-# class TestCase:
-#     """
-#         Class that represents test case
-#         :param sql: sql query
-#         :type: sql: str
-#         :param: expected_value: expected value that query should return
-#         :type expected_value: `typing.Any`
-#     """
-#     sql: str
-#     expected_value: 'typing.Any'
+@dataclass
+class TestCase:
+    """
+        Class that represents test case
+        :param sql: sql query
+        :type: sql: str
+        :param: expected_value: expected value that query should return
+        :type expected_value: `typing.Any`
+    """
+    sql: str
+    expected_value: 'typing.Any'
